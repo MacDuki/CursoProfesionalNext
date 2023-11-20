d290d5982af9acd8f4808b231d624b9e7e5b9469
MacDuki <PCMYACT@hotmail.com>
2023-11-20 17:31:44 -0300

    Agrego price section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import { Feature } from "components/FeatureSec/Feature";
+import { Price } from "components/Price/Price";
 import { Hero } from "../components/Hero/Hero";
 export default function Home() {
 	return (
 		<>
 			<Hero></Hero>
 			<Feature></Feature>
+			<Price></Price>
 		</>
 	);
 }
